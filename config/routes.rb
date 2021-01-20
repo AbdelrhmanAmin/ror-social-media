16751df518d4c2a9d07db3935f8e03009f16540b
AbdelrhmanAmin <maxhd213@gmail.com>
2021-01-20 23:34:55 +0200

    Add assosications and migration to friendship db

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   root 'posts#index'
 
   devise_for :users
-
-  resources :users, only: [:index, :show]
+  resources :friendships
+  resources :users
   resources :posts, only: [:index, :create] do
     resources :comments, only: [:create]
     resources :likes, only: [:create, :destroy]
